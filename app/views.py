6478bb84d871796eea9df6b93ad6522599560706
briankc804 <briankc804@gmail.com>
2022-05-01 22:29:56 +0300

    added for loop

--- app/views.py
+++ app/views.py
@@ -9,10 +9,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-
-    title = 'Welcome to the ultimate news review site'
+    #Getting business news
+    business_news =get_news('business')
+    entertainment_news=get_news('entertainment')
+    technology_news=get_news('technology')
+    title = 'Home-Welcome to the ultimate news review site'
     
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title,business = business_news,entertainment =entertainment_news,technology =technology_news)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
